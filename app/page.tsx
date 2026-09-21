610fd6cc123438058a16e2bba553c51280c44795
Tahsin Nayeem Shrestha <144557488+Tshrestha-1114@users.noreply.github.com>
2026-09-21 13:38:32 -0400

    Build kingdom portfolio foundation

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import { Hero } from "@/components/sections/Hero";
-import { About } from "@/components/sections/About";
-import { Skills } from "@/components/sections/Skills";
-import { Experience } from "@/components/sections/Experience";
-import { Projects } from "@/components/sections/Projects";
-import { Extracurriculars, Contact } from "@/components/sections/Extracurriculars";
+import { KingdomPortfolio } from "@/components/kingdom/KingdomPortfolio";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Skills />
-      <Extracurriculars />
-      <Contact />
-    </>
-  );
+  return <KingdomPortfolio />;
 }
